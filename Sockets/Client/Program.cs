24080354f795865f621c7b8c0a152566064227b0
gavinpullen <pullengavin@gmail.com>
2014-01-19 12:11:12 +0000

    Added new source code

---
+++ Sockets/Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Client
+{
+    class Program
+    {
+        static Socket c_socket;
+
+
+        static void Main(
+            string[] args)
+        {
+            c_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint _endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            try
+            {
+                c_socket.Connect(_endPoint);
+            }
+            catch
+            {
+                
+                Console.WriteLine("Unable to connect to local endpoint!!!!");
+                Main(args);
+            }
+
+            Console.WriteLine("Enter text : ");
+
+            string _input = Console.ReadLine();
+
+            byte[] _data = Encoding.ASCII.GetBytes(_input);
+
+            c_socket.Send(_data);
+
+            Console.WriteLine("Data sent!!!!");
+            Console.WriteLine("Press any key to continue.....");
+            Console.ReadLine();
+
+            c_socket.Close();
+        }
+    }
+}
---
+++ Sockets/Sockets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Sockets
+{
+    class Program
+    {
+        static byte[] c_buffer { get; set; }
+        static Socket c_socket;
+
+
+        static void Main(
+            string[] args)
+        {
+            c_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            c_socket.Bind(new IPEndPoint(0, 1234));
+
+            c_socket.Listen(100);
+
+            Socket _acceptedSocket = c_socket.Accept();
+
+            c_buffer = new byte[_acceptedSocket.SendBufferSize];
+
+            int _bytesRead = _acceptedSocket.Receive(c_buffer);
+
+            byte[] _formatted = new byte[_bytesRead];
+
+            for (int i = 0; i < _bytesRead; i++)
+            {
+                _formatted[i] = c_buffer[i];   
+            }
+
+            string _dataString = Encoding.ASCII.GetString(_formatted);
+
+            Console.WriteLine(_dataString + Environment.NewLine);
+            Console.ReadLine();
+
+            c_socket.Close();
+            _acceptedSocket.Close();
+
+        }
+    }
+}
